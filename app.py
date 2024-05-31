898c6f37e30fd0a6aaea8d5ce56f4e23d126389f
imsyc <songyuchen2003@hotmail.com>
2024-05-31 18:25:42 +0300

    Fix imports

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
 from flask import Flask
-from flask import redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
-from os import getenv
-from sqlalchemy.sql import text
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+app.secret_key = '5364d23940c3ec8d93a8a07bd8a423f4'
 
-@app.route("/")
-def index():
-    db.session.execute(text("INSERT INTO visitors (time) VALUES (NOW())"))
-    db.session.commit()
-    result = db.session.execute(text("SELECT COUNT(*) FROM visitors"))
-    counter = result.fetchone()[0]
-    return render_template("index.html", counter=counter) 
+import routes
