17a7a6b2b06cd19ffb83ab323f096c0adb570381
imsyc <songyuchen2003@hotmail.com>
2024-06-01 23:58:08 +0300

    add restaurants

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from db import db
-from werkzeug.security import generate_password_hash, check_password_hash
+from db import db # type: ignore
+from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = 'users' 
@@ -14,3 +15,32 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    restaurant_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    address = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    opening_hours = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'<Restaurant {self.name}>'
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    
+    review_id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.restaurant_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id')) 
+    rating = db.Column(db.Float)
+    comment = db.Column(db.String(1024))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Review {self.review_id} by User {self.user_id}>'
+
+
