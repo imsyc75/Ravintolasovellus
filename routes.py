ee9ec0c51cc97541d8ee942bdf86e671856b9eff
imsyc <songyuchen2003@hotmail.com>
2024-06-10 22:32:21 +0300

    delete models

--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
-from flask import render_template, request, redirect, session, jsonify # type: ignore
+from flask import request, render_template, redirect, session # type: ignore
 from app import app # type: ignore
-from db import db # type: ignore
-from models import User, Restaurant,Review# type: ignore
+from db import get_db_connection # type: ignore
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -14,17 +15,31 @@ def register():
     elif request.method == 'POST':
         name = request.form['name']
         password = request.form['password']
-        role = 1  # Defaults to 1, and can be set by the administrator.
-        
-        if User.query.filter_by(name=name).first():
-            return 'Username already exists', 400
-        
-        new_user = User(name=name, role=role)
-        new_user.set_password(password)
-        db.session.add(new_user)
-        db.session.commit()
+        role = 1 # Defaults to 1, and can be set by the administrator.
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        try:
+            sql = """SELECT * FROM users WHERE name = %s"""
+            cur.execute(sql, (name,))
+            if cur.fetchone():
+                return 'Username already exists', 400
+            
+            sql = """INSERT INTO users (name, password, role) 
+            VALUES (%s, crypt(%s, gen_salt('bf')), %s)"""
+            cur.execute(sql ,(name, password, role))
+            conn.commit()
+
+        except Exception as e:
+            conn.rollback()
+            return str(e), 500
+        finally:
+            cur.close()
+            conn.close()
 
         return redirect('/login')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,61 +48,97 @@ def login():
     elif request.method == 'POST':
         name = request.form['name']
         password = request.form['password']
-        
-        user = User.query.filter_by(name=name).first()
-        if user and user.check_password(password):
-            session['username'] = user.name
-            return redirect('/restaurants')
-        else:
-            return 'Invalid username or password, please try again.', 401
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        try:
+            sql = """SELECT password FROM users WHERE name = %s 
+            AND password = crypt(%s, password)"""
+            cur.execute(sql, (name, password))
+            if cur.fetchone():
+                session['username'] = name
+                return redirect('/')
+            else:
+                return 'Invalid username or password, please try again.', 401
+        except Exception as e:
+            return str(e), 500
+        finally:
+            cur.close()
+            conn.close()
+
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect('/')
 
+
 @app.route('/restaurants')
 def restaurants():
-    restaurant_list = Restaurant.query.all()  
+    conn = get_db_connection()
+    cur = conn.cursor()
+    sql = """SELECT restaurant_id, name, latitude, longitude, 
+            description, address, opening_hours 
+            FROM restaurants"""
+    cur.execute(sql)
+
+    restaurant_list = cur.fetchall()
     restaurants = [
         {
-            "restaurant_id": restaurant.restaurant_id, 
-            "name": restaurant.name,
-            "lat": restaurant.latitude,
-            "lon": restaurant.longitude,
-            "info": f"{restaurant.description}<br>{restaurant.address}<br>opening hours:{restaurant.opening_hours}"
+            "restaurant_id": restaurant[0],
+            "name": restaurant[1],
+            "lat": restaurant[2],
+            "lon": restaurant[3],
+            "info": f"{restaurant[4]}<br>{restaurant[5]}<br>opening hours:{restaurant[6]}"
         } for restaurant in restaurant_list
     ]
+    cur.close()
+    conn.close()
     return render_template('restaurants.html', restaurants=restaurants)
+
 
 #see the restaurants information
 @app.route('/restaurant/<int:restaurant_id>')
 def restaurant_detail(restaurant_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    sql = """SELECT * FROM restaurants WHERE restaurant_id = %s"""
+    cur.execute(sql, (restaurant_id,))
+    restaurant = cur.fetchone()
 
-    restaurant = Restaurant.query.get(restaurant_id)
     if not restaurant:
         return "Restaurant not found", 404
     
-    reviews = Review.query.filter_by(restaurant_id=restaurant_id).all()
+    sql = """ SELECT r.*, u.name  
+              FROM reviews r JOIN users u ON r.user_id = u.id 
+              WHERE r.restaurant_id = %s"""
+    cur.execute(sql, (restaurant_id,))
+    reviews = cur.fetchall()
+
+    cur.close()
+    conn.close()
     return render_template('restaurant_detail.html', restaurant=restaurant, reviews=reviews)
+
 
 #add review of restaurants
 @app.route('/restaurant/<int:restaurant_id>/add_review', methods=['POST'])
 def add_review(restaurant_id):
     if 'username' not in session:
         return redirect('/login')
-    user = User.query.filter_by(name=session['username']).first()
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    sql = """""SELECT id FROM users WHERE name = %s"""
+    cur.execute(sql, (session['username'],))
+    user = cur.fetchone()
     if not user:
         return 'User not found', 404
 
     comment = request.form['comment']
     rating = float(request.form['rating'])
-    new_review = Review(
-        restaurant_id=restaurant_id,
-        user_id=user.id,
-        rating=rating,
-        comment=comment
-    )
-    db.session.add(new_review)
-    db.session.commit()
+    sql = """"INSERT INTO reviews (restaurant_id, user_id, rating, comment) VALUES (%s, %s, %s, %s)""""
+    cur.execute(sql,(restaurant_id, user[0], rating, comment))
+    conn.commit()
+    cur.close()
+    conn.close()
     return redirect(f'/restaurant/{restaurant_id}')
